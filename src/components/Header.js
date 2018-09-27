dba3673afaa4f582d3e52b408dec8d922886d83a
Alejandro Velásquez Molina <alejandrovelasquezmolina@gmail.com>
2018-09-26 21:56:41 -0500

    Header

---
+++ src/components/Header.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+const Header = (props) => {
+  return (
+    <nav>
+      <div className="nav-wrapper light-blue darken-3">
+          <a href="" 
+            className="brand-logo center"
+          > {props.titulo} </a>
+      </div>
+    </nav>
+  )
+}
+
+export default Header;
