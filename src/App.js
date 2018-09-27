a6f2fc594080e5e397b3769ad7bd9c547ddcce36
Alejandro Velásquez Molina <alejandrovelasquezmolina@gmail.com>
2018-09-27 05:24:00 -0500

    Guardando noticias en el state

--- src/App.js
+++ src/App.js
@@ -3,6 +3,31 @@ import React, { Component } from 'react';
 import Header from './components/Header';
 
 class App extends Component {
+
+  state = {
+    noticias: []
+  }
+
+  componentDidMount() {
+    this.consultarNoticias();
+  }
+
+  consultarNoticias = () => {
+    let pais = 'CO';
+    let url = `https://newsapi.org/v2/top-headlines?country=${pais}&category=business&apiKey=5099b2a1a18e40ceab56b16da3aa8ac3`;
+
+    fetch(url)
+      .then(respuesta => {
+        return respuesta.json()
+      })
+      .then(noticias => {
+        this.setState({
+          noticias : noticias.articles
+        })
+      } )
+    
+  }
+
   render() {
     return (
       <div className="App">
