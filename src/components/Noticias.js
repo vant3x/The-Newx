1858a15eacfad08f97d2b536c5b16ab98cf47fdc
Alejandro Velásquez Molina <alejandrovelasquezmolina@gmail.com>
2018-09-28 14:30:51 -0500

    Agregando animación al cambio de categoría

--- src/components/Noticias.js
+++ src/components/Noticias.js
@@ -1,18 +1,24 @@
 import React from 'react';
 import Noticia from './Noticia';
 import PropTypes from 'prop-types';
+import {TransitionGroup, CSSTransition} from 'react-transition-group';
 
 class Noticias extends React.Component {
   
   render() {
     return (
       <div className="row">
-        {this.props.noticias.map(noticia => (
-          <Noticia 
-            key={noticia.url}
-            noticia={noticia}
-          />
-        ))}
+        <TransitionGroup>
+          {this.props.noticias.map(noticia => (
+            <CSSTransition
+              key={noticia.url}
+              classNames="fade"
+              timeout={500}
+            >
+              <Noticia noticia={noticia} />
+            </CSSTransition>
+          ))}
+        </TransitionGroup>
       </div>
     );
   }
